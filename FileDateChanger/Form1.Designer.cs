dc4f93affbee6859851a864f8fe6494e2ebbc137
Jesus <Jesus@INDIGO>
2023-05-25 19:59:46 +0300

    Add project files.

---
+++ FileDateChanger/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace FileDateChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.materialSwitch1 = new MaterialSkin.Controls.MaterialSwitch();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(8, 65);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(333, 132);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 195);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 195);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(10, 328);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(331, 33);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Change Date";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 245);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Creation Time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 272);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Last Write Time:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 299);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Last Access Time:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(140, 242);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(108, 22);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(140, 269);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(108, 22);
+            this.dateTimePicker2.TabIndex = 12;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker3.Location = new System.Drawing.Point(140, 296);
+            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(108, 22);
+            this.dateTimePicker3.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(259, 298);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 272);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 245);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
+            // 
+            // materialSwitch1
+            // 
+            this.materialSwitch1.AutoSize = true;
+            this.materialSwitch1.Depth = 0;
+            this.materialSwitch1.Location = new System.Drawing.Point(3, 24);
+            this.materialSwitch1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch1.Name = "materialSwitch1";
+            this.materialSwitch1.Ripple = true;
+            this.materialSwitch1.Size = new System.Drawing.Size(107, 37);
+            this.materialSwitch1.TabIndex = 18;
+            this.materialSwitch1.Text = "Theme";
+            this.materialSwitch1.UseVisualStyleBackColor = true;
+            this.materialSwitch1.CheckedChanged += new System.EventHandler(this.MaterialSwitch1_CheckedChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renameToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(118, 26);
+            // 
+            // renameToolStripMenuItem
+            // 
+            this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.renameToolStripMenuItem.Text = "Rename";
+            this.renameToolStripMenuItem.Click += new System.EventHandler(this.renameToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 317);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 374);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.materialSwitch1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3, 24, 4, 4);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FileDateChanger/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.IO;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace FileDateChanger
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.AllowDrop = true;
+            listBox1.DragDrop += ListBox1_DragDrop;
+            listBox1.DragEnter += ListBox1_DragEnter;
+            listBox1.MouseDoubleClick += ListBox1_MouseDoubleClick;
+            listBox1.MouseDown += ListBox1_MouseDown;
+            listBox1.SelectedIndexChanged += ListBox1_SelectedIndexChanged;
+            listBox1.KeyDown += ListBox1_KeyDown;
+
+            textBox1.LostFocus += TextBox1_LostFocus;
+            textBox1.KeyDown += TextBox1_KeyDown;
+
+            dateTimePicker1.ValueChanged += (s, e) => ShowDate();
+            dateTimePicker2.ValueChanged += (s, e) => ShowDate();
+            dateTimePicker3.ValueChanged += (s, e) => ShowDate();
+
+            themeManager = MaterialSkinManager.Instance;
+            themeManager.AddFormToManage(this);
+            themeManager.ColorScheme = new ColorScheme
+                (
+                Primary.Blue500,
+                Primary.Blue500,
+                Primary.Blue500,
+                Accent.LightBlue200,
+                TextShade.BLACK
+                );
+        }
+
+
+        private readonly INIFile INI = new INIFile();
+        private readonly MaterialSkinManager themeManager = null;
+        private int selectedIndex = -1;
+        private bool editText = false;
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Enter:
+                    EndEdit();
+                    break;
+
+                case Keys.Escape:
+                    editText = false;
+                    EndEdit();
+                    break;
+            }
+        }
+
+        private void TextBox1_LostFocus(object sender, EventArgs e)
+        {
+            if (editText)
+            {
+                EndEdit();
+            }
+        }
+
+        private void BeginEdit()
+        {
+            if (selectedIndex >= 0)
+            {
+                Rectangle rectangle = listBox1.GetItemRectangle(selectedIndex);
+                textBox1.Visible = true;
+                textBox1.Location = new Point(listBox1.Left + rectangle.X, listBox1.Top + rectangle.Y);
+                textBox1.Width = rectangle.Width + 2;
+                textBox1.Text = Path.GetFileNameWithoutExtension(listBox1.Items[selectedIndex].ToString());
+                textBox1.Focus();
+                editText = true;
+            }
+        }
+
+        private void EndEdit()
+        {
+            if (selectedIndex >= 0)
+            {
+                if (editText)
+                {
+                    string oldFullFileName = listBox1.Items[selectedIndex].ToString();
+                    string oldName = Path.GetFileNameWithoutExtension(oldFullFileName);
+                    string newName = textBox1.Text;
+
+                    if (newName.Length > 0 && oldName != newName)
+                    {
+                        string newFullFileName = oldFullFileName.Replace(oldName, newName);
+                        listBox1.Items[selectedIndex] = newFullFileName;
+
+                        try
+                        {
+                            File.Move(oldFullFileName, newFullFileName);
+                        }
+                        catch (Exception exc)
+                        {
+                            MessageBox.Show(exc.Message);
+                        }
+                    }
+                }
+
+                editText = false;
+                textBox1.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowDate();
+            Location = new Point(INI.Parse("Main", "X"), INI.Parse("Main", "Y"));
+            materialSwitch1.Checked = INI.Read("Main", "Theme").Equals("DARK");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            INI.Write("Main", "X", Location.X);
+            INI.Write("Main", "Y", Location.Y);
+            INI.Write("Main", "Theme", themeManager.Theme);
+        }
+
+        private void ListBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && listBox1.Items.Count > 0)
+            {
+                contextMenuStrip1.Show(MousePosition);
+            }
+        }
+
+        private void ListBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop, false) ? DragDropEffects.Copy : DragDropEffects.None;
+        }
+
+        private void ListBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string[] files = GetAllFiles(paths);
+            AddItems(files);
+        }
+
+        private void ShowDate()
+        {
+            label4.Text = dateTimePicker1.Value.ToShortDateString();
+            label5.Text = dateTimePicker2.Value.ToShortDateString();
+            label6.Text = dateTimePicker3.Value.ToShortDateString();
+        }
+
+        private void ResetDate()
+        {
+            dateTimePicker1.ResetText();
+            dateTimePicker2.ResetText();
+            dateTimePicker3.ResetText();
+        }
+
+        private void ReadFileDate(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+
+            dateTimePicker1.Value = fileInfo.CreationTime;
+            dateTimePicker2.Value = fileInfo.LastWriteTime;
+            dateTimePicker3.Value = fileInfo.LastAccessTime;
+
+            ShowDate();
+        }
+
+        private void WriteFileDate(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+
+            FileAttributes oldAttributes = fileInfo.Attributes;
+            fileInfo.Attributes = FileAttributes.Normal;
+
+            fileInfo.CreationTime = dateTimePicker1.Value;
+            fileInfo.LastWriteTime = dateTimePicker2.Value;
+            fileInfo.LastAccessTime = dateTimePicker3.Value;
+
+            fileInfo.Attributes = oldAttributes;
+        }
+
+        private void ReadFolderDate(string folderName)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderName);
+
+            dateTimePicker1.Value = directoryInfo.CreationTime;
+            dateTimePicker2.Value = directoryInfo.LastWriteTime;
+            dateTimePicker3.Value = directoryInfo.LastAccessTime;
+
+            ShowDate();
+        }
+
+        private void WriteFolderDate(string folderName)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderName);
+
+            FileAttributes oldAttributes = directoryInfo.Attributes;
+            directoryInfo.Attributes = FileAttributes.Normal;
+
+            directoryInfo.CreationTime = dateTimePicker1.Value;
+            directoryInfo.LastWriteTime = dateTimePicker2.Value;
+            directoryInfo.LastAccessTime = dateTimePicker3.Value;
+
+            directoryInfo.Attributes = oldAttributes;
+        }
+
+        private void ListBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                string name = listBox1.SelectedItem.ToString();
+                System.Diagnostics.Process.Start(name);
+            }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex >= 0 && !editText)
+            {
+                string name = listBox1.SelectedItem.ToString();
+                if (File.Exists(name))
+                {
+                    ReadFileDate(name);
+                }
+                else
+                {
+                    ReadFolderDate(name);
+                }
+            }
+        }
+
+        private void ListBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+
+            if (e.KeyCode == Keys.F3)
+            {
+                BeginEdit();
+            }
+            else if (index != -1 && e.KeyCode == Keys.Delete)
+            {
+                listBox1.Items.RemoveAt(index);
+            }
+        }
+
+        private string[] GetAllFiles(params string[] paths)
+        {
+            List<string> result = new List<string>();
+            foreach (string path in paths)
+            {
+                result.AddRange(GetFilesAndDirs(path));
+            }
+            return result.ToArray();
+        }
+
+        private List<string> GetFilesAndDirs(string path)
+        {
+            List<string> result = new List<string>();
+
+            if (File.Exists(path))
+            {
+                result.Add(path);
+            }
+            else
+            {
+                result.Add($"{path}\\");
+                foreach (string item in Directory.GetFileSystemEntries(path))
+                {
+                    if (File.Exists(item))
+                    {
+                        result.Add(item);
+                    }
+                    else
+                    {
+                        result.AddRange(GetFilesAndDirs(item));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void AddItems(params string[] files)
+        {
+            foreach (string file in files)
+            {
+                if (!listBox1.Items.Contains(file))
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+
+            if (listBox1.Items.Count > 0 && listBox1.SelectedIndex < 0)
+            {
+                listBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                AddItems(openFileDialog1.FileNames);
+            }
+            else
+            {
+                MaterialSnackBar snackBar = new MaterialSnackBar("No files selected", 1300);
+                snackBar.Show(this);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            ResetDate();
+            MaterialSnackBar snackBar = new MaterialSnackBar("List cleared", 1300);
+            snackBar.Show(this);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                string name = listBox1.SelectedItem.ToString();
+                if (File.Exists(name))
+                {
+                    WriteFileDate(name);
+                }
+                else
+                {
+                    WriteFolderDate(name);
+                }
+
+                MaterialSnackBar snackBar = new MaterialSnackBar("Done", 1300);
+                snackBar.Show(this);
+            }
+            else
+            {
+                MaterialSnackBar snackBar = new MaterialSnackBar("No selected item", 1300);
+                snackBar.Show(this);
+            }
+        }
+
+        private void MaterialSwitch1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (materialSwitch1.Checked)
+            {
+                themeManager.Theme = MaterialSkinManager.Themes.DARK;
+            }
+            else
+            {
+                themeManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            }
+        }
+
+        private void renameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BeginEdit();
+        }
+    }
+}
